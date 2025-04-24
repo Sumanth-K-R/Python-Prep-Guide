efc954013249ee74d993fe379c0992c5169c0448
Sumanth <sumanthkr.work@gmail.com>
2025-04-24 11:07:28 +0530

    added a prob statement and solution for inheritance

--- OOPS/Inheritance.py
+++ OOPS/Inheritance.py
@@ -1,3 +1,4 @@
+"""Sample Inheritance Demonstration"""
 # Single Inheritance
 
 class A:
@@ -29,3 +30,66 @@ class E(C, D):
 
 obj2 = E()
 
+
+#---------------------------------------------------------------------------------------------------------------------
+
+
+
+"""
+Problem Statement:
+Design a class hierarchy for a system that manages users of an application. There is a base class called User that contains:
+
+Attributes: username, email
+
+Method: display_info()
+
+Create two subclasses:
+
+Admin with an additional method view_dashboard() that prints "Admin Dashboard Accessed"
+
+Customer with an additional method view_products() that prints "Product List Viewed"
+
+admin = Admin("admin_user", "admin@example.com")
+customer = Customer("cust_user", "customer@example.com")
+
+admin.display_info()        # Username: admin_user, Email: admin@example.com
+admin.view_dashboard()      # Admin Dashboard Accessed
+
+customer.display_info()     # Username: cust_user, Email: customer@example.com
+customer.view_products()    # Product List Viewed
+
+"""
+
+class User:
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+class Admin(User):
+    def __init__(self, username, email):
+        super().__init__(username, email)
+
+    def display_info(self):
+        print(self.username, self.email)
+
+    def view_dashboard(self):
+        print("Admin Dashboard Accessed")
+
+class Customer(User):
+    def __init__(self, username, email):
+        super().__init__(username, email)
+
+    def view_products(self):
+        print("Product List Viewed")
+
+    def display_info(self):
+        print(self.username, self.email)
+
+admin = Admin("admin_user", "admin@example.com")
+customer = Customer("cust_user", "customer@example.com")
+
+admin.display_info()
+admin.view_dashboard()
+
+customer.display_info()
+customer.view_products()
